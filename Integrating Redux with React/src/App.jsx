22c56868af492038556408b625c8b52eeb449db2
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-05-11 15:18:44 +0530

    Integrating Redux With React

---
+++ Integrating Redux with React/src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment, useState } from 'react';
+import Counter from './components/Counter';
+import './App.css';
+
+function App() {
+
+  return (
+    <Fragment>
+      <Counter />
+    </Fragment>
+  )
+}
+
+export default App;
